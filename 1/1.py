f9e209b19d7498990ce227fbf4c3f276849978af
Mohit Kumhar <mohitmolela@gmail.com>
2024-05-22 23:04:58 +0530

    introduction to Data structure and algo

---
+++ 1/1.py
@@ -0,0 +1,8 @@
+# Data structures algorithms are two different things
+
+# Data Structure - Arrangement of data so that they can be used efficiently in memory
+
+# Algorithm - Sequence of steps on the data using efficient data structure to solve the given problem
+
+
+
